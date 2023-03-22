90b1997d249836bbcd1688187a5913e0872275ee
Jhonatta Rodrigues <107954865+JhowRodrigues2@users.noreply.github.com>
2023-03-21 21:12:39 -0300

    create and inststylization footer component

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -3,7 +3,7 @@ import { FooterContainer } from "./styles";
 export const Footer = () => {
   return (
     <FooterContainer>
-      <span>Marvel ©</span>
+      <span>Marvel 2023 ©</span>
       <a href="https://github.com/JhowRodrigues2">Jr Developer</a>
     </FooterContainer>
   );
