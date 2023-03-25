fb2227f442f45f804c37c33ae1dd007b5e300f8d
Jhonatta Rodrigues <107954865+JhowRodrigues2@users.noreply.github.com>
2023-03-24 21:07:08 -0300

    create in context fetch marvel api

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,5 +1,6 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 export const GlobalContext = createContext({});
+import md5 from "md5";
 
 const GlobalProvider = ({ children }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(true);
@@ -7,14 +8,29 @@ const GlobalProvider = ({ children }) => {
   const publicKey = import.meta.env.VITE_API_PUBLIC_KEY;
   const privateKey = import.meta.env.VITE_API_PRIVATE_KEY;
 
-  console.log(publicKey, privateKey);
+  const [comicsDATA, setComicsDATA] = useState([]);
+
+  useEffect(() => {
+    async function loadHeroesCharacters() {
+      const timestamp = new Date().getTime();
+      const hash = md5(timestamp + privateKey + publicKey);
+
+      const req = await fetch(
+        `https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&orderBy=name&limit=10&apikey=${publicKey}&hash=${hash}`
+      );
+      const res = await req.json();
+      setComicsDATA(res);
+    }
+    loadHeroesCharacters();
+  }, []);
+  console.log(comicsDATA);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
   return (
-    <GlobalContext.Provider value={{ isMenuOpen, toggleMenu }}>
+    <GlobalContext.Provider value={{ isMenuOpen, toggleMenu, comicsDATA }}>
       {children}
     </GlobalContext.Provider>
   );
